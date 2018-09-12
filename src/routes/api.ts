9d1c917891d2827ac26dcc1005f1f1fcd2f5c17b
Yauheniya Litvin <Yauheniya_Litvin@epam.com>
2018-09-12 14:49:04 +0300

    #7 api validation

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,17 +1,18 @@
 import {Request, Response, NextFunction} from 'express'
 import * as dbInstance from './../dbInstance'
 import { getStrict }  from '../storage/mongo/show'
+import {validateQuety} from './validator'
 
 export async function get(req:Request, res:Response, next:NextFunction) {
 
   try {   
    
-    const {limit = 3,page = 1} = req.query; 
-    
-        
+    if( !validateQuety(req.query) ) throw new Error('invalid query params')
+      
+    const {limit = 3,page = 1} = req.query          
     const db = await dbInstance.get()
     const shows = await getStrict(db , +page, +limit)
-    res.status(200).json( shows );
+    res.status(200).json( shows )
 
   } catch (err) {
     next(err);
--- src/routes/shows.ts
+++ src/routes/shows.ts
@@ -1,13 +1,15 @@
 
 import {Request, Response, NextFunction} from 'express';
 import { get as getShow }  from '../storage/mongo/show';
-
+import {validateQuety} from './validator'
 import * as dbInstance from './../dbInstance'
 
 export async function get(req:Request, res:Response, next:NextFunction) {
 
-  try {   
-
+  try {       
+   
+    if( !validateQuety(req.query) ) throw new Error('invalid query params')
+    
     const {limit = 3,page = 1} = req.query; 
     const db = await dbInstance.get()
     const shows = await getShow(db , +page, +limit)
@@ -18,4 +20,3 @@ export async function get(req:Request, res:Response, next:NextFunction) {
   }
 
 }
-
