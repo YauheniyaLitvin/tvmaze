4541832abc948c7a49dcfed6e082d6796648ab66
Yauheniya Litvin <Yauheniya_Litvin@epam.com>
2018-09-14 17:14:40 +0300

    Fixed #9

--- src/logger.ts
+++ src/logger.ts
@@ -1,42 +1,42 @@
-import  { createLogger, format, transports, Logger } from 'winston'
-const { combine, timestamp, printf, label } = format;
 import fs from 'fs'
-
+import  { createLogger, format, transports } from 'winston'
+const { combine, timestamp, printf, label } = format
+const env = process.env.NODE_ENV || 'development'
 
 const logdir:string = 'log'
 if (!fs.existsSync(logdir)) {
     fs.mkdirSync(logdir);
 }
 
-
-const myFormat = printf(info => {
+const consoleTransport = new transports.Console({format:format.simple(), level:'debug'})
+const combinedTransport = new transports.File({ filename: logdir+'/combined.log' })
+const fileFormat = printf(info => {
     return `${info.timestamp} ${info.level}: ${info.message} [${info.label}]` ;
-});
+})
 
-const logger:Logger = createLogger({
-    format:combine(
-        timestamp(),
-        myFormat
-    ),
-    transports: [
-        new transports.File({ filename: logdir+'/error.log', level: 'error' }),
-        new transports.File({ filename: logdir+'/combined.log' })
-    ]
-});
+function newLogger( filename:string ){
 
-
-export default  ( callmodule:NodeModule )=> {
-
-    return createLogger({
-        format:combine(
-            label({ label:  callmodule.filename }),
-            timestamp(),            
-            myFormat
-        ),
+    const logger = createLogger({ 
+        format: getFormat(filename ),   
         transports: [
-            new transports.File({ filename: logdir+'/error.log', level: 'error' }),
-            new transports.File({ filename: logdir+'/combined.log' })
-        ]
-    });
+                new transports.File({ filename: logdir+'/error.log', level: 'error' })
+        ]    
+    })
+    if (process.env.NODE_ENV !== 'production') {
+        logger.add(  consoleTransport )
+        logger.add(  combinedTransport )
+    }    
+    return logger 
+}
 
+function getFormat( filename:string ){
+    return combine(
+        label({ label: filename }),
+        timestamp(),            
+        fileFormat
+    )
+}
+
+export default  ( callmodule:NodeModule )=> {    
+    return newLogger(callmodule.filename)
 }
